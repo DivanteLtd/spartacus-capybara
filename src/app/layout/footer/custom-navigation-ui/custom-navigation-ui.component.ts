fa2c9d7a4ad3d13f126e4a0941b713619afad66d
Michal Dydo <mdydo@divante.pl>
2020-03-03 12:16:00 +0100

    Refactor

--- src/app/layout/footer/custom-navigation-ui/custom-navigation-ui.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { NavigationUIComponent } from '@spartacus/storefront';
-
-@Component({
-  selector: 'app-custom-navigation-ui',
-  templateUrl: './custom-navigation-ui.component.html',
-  styleUrls: ['./custom-navigation-ui.component.scss'],
-})
-export class CustomNavigationUiComponent extends NavigationUIComponent {}
--- src/app/layout/footer/footer.module.ts
+++ src/app/layout/footer/footer.module.ts
@@ -1,22 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UrlModule, I18nModule } from '@spartacus/core';
 
 import { CustomFooterComponent } from './custom-footer/custom-footer.component';
-import { CustomNavigationUiComponent } from './custom-navigation-ui/custom-navigation-ui.component';
-import { IconModule, GenericLinkModule } from '@spartacus/storefront';
 
 @NgModule({
-  declarations: [CustomFooterComponent, CustomNavigationUiComponent],
-  imports: [
-    CommonModule,
-
-    // SPRATACUS
-    UrlModule,
-    GenericLinkModule,
-    IconModule,
-    I18nModule,
-  ],
+  declarations: [CustomFooterComponent],
+  imports: [CommonModule],
   entryComponents: [CustomFooterComponent],
 })
 export class FooterModule {}
