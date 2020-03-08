e7f2b718ef8baa50b4695ac9258ccdbdf0d25284
Adam Kaczmarek <akaczmarek@divante.com>
2020-03-08 14:07:34 +0100

    SSD-Changed style guide component class name and selector to correct.

--- src/app/features/style-guide/style-guide.module.ts
+++ src/app/features/style-guide/style-guide.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StyleGuildeComponent } from './style-guilde/style-guilde.component';
+import { StyleGuideComponent } from './style-guide/style-guide.component';
 import { RouterModule } from '@angular/router';
 import { styleGuideRoutes } from './style-guide.routes';
 
 @NgModule({
-  declarations: [StyleGuildeComponent],
+  declarations: [StyleGuideComponent],
   imports: [CommonModule, RouterModule.forChild(styleGuideRoutes)],
 })
 export class StyleGuideModule {}
