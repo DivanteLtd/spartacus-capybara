fc1e5832ba5fde534778df312daa7df763aa09a3
Adam Kaczmarek <akaczmarek@divante.com>
2020-03-09 13:27:22 +0100

    SSD-50 Added select styles (for native and ng-select). Style guide page updated.

--- src/app/features/style-guide/style-guide.module.ts
+++ src/app/features/style-guide/style-guide.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 import { StyleGuideComponent } from './style-guide/style-guide.component';
 import { RouterModule } from '@angular/router';
 import { styleGuideRoutes } from './style-guide.routes';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   declarations: [StyleGuideComponent],
-  imports: [CommonModule, RouterModule.forChild(styleGuideRoutes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(styleGuideRoutes),
+    NgSelectModule,
+  ],
 })
 export class StyleGuideModule {}
