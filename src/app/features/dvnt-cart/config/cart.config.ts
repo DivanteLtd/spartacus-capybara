6fee7a03bfdc7dc9cd2ad2187766fa31a899aa56
Michal Dydo <mdydo@divante.pl>
2020-04-09 11:28:15 +0200

    Resolving problem with translation with --aot flag

--- src/app/features/dvnt-cart/config/cart.config.ts
+++ src/app/features/dvnt-cart/config/cart.config.ts
@@ -1,24 +1,18 @@
-import { translationChunksConfig } from '@spartacus/assets';
-
 const cartTranslationOverwrites = {
   en: {
     cart: {
-      cartColumn: {
+      cartDetails: {
+        total: 'Total items: {{count}}',
+        goToCheckout: 'Go to checkout',
         discount: 'Discount',
         grandTotal: 'Grand total',
         shippingAndHandling: 'Shipping & Handling (flat rate - fixed)',
         subtotal: 'Subtotal',
         tax: 'Tax',
-      },
-      empty: {
-        slogan:
+        emptySlogan:
           'Looks like you haven’t added any items to the bag yet. Start shopping to fill it in.',
         startShopping: 'Start shopping',
-        title: 'Your bag is empty',
-      },
-      filled: {
-        total: 'Total items: {{count}}',
-        goToCheckout: 'Go to checkout',
+        emptytitle: 'Your bag is empty',
       },
     },
   },
@@ -27,12 +21,5 @@ const cartTranslationOverwrites = {
 export const cartTranslationsConfig = {
   i18n: {
     resources: cartTranslationOverwrites,
-    chunks: {
-      common: translationChunksConfig.cart.push(
-        'cartColumn',
-        'empty',
-        'filled'
-      ),
-    },
   },
 };
