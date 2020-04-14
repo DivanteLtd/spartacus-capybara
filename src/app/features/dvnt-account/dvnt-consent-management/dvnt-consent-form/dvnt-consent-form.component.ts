5576c3065368a44d51b2d5cb2d20162d848ee1f7
Michał D <mdydo@divante.pl>
2020-04-14 11:31:34 +0200

    GH-23 recreate consents management page (#34)

---
+++ src/app/features/dvnt-account/dvnt-consent-management/dvnt-consent-form/dvnt-consent-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ConsentManagementFormComponent } from '@spartacus/storefront';
+
+@Component({
+  selector: 'app-dvnt-consent-form',
+  templateUrl: './dvnt-consent-form.component.html',
+  styleUrls: ['./dvnt-consent-form.component.scss'],
+})
+export class DvntConsentFormComponent extends ConsentManagementFormComponent {}
